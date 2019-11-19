95a708ca88236b9a8c01eab2b9331b2c41105dfe
Mario Petkovski <mario5etkovski@gmail.com>
2019-11-19 21:49:41 +0100

    First Commit
    Intro to React and creating our first Component.

---
+++ first-app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Hello } from "./Hello";
+
+export class App extends React.Component {
+    render() {
+        return (
+           <div>
+                <h2>Mario Petkovski</h2>
+                <Hello />
+           </div>
+        )
+    }
+}
---
+++ first-app/src/components/Hello.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Hello extends React.Component {
+    render() {
+        return (
+            <h2>Hello Mario, welcome to React.</h2>
+        )
+    }
+}
